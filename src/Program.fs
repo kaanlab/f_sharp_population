79186848d5cdf338ef8368682058774c5b649fec
Anton Kantyshev <kaan@live.ru>
2023-05-24 17:24:40 +0300

    add user input

--- src/Program.fs
+++ src/Program.fs
@@ -4,6 +4,7 @@ open FSharp.Data
 open FsToolkit.ErrorHandling.ValidationCE
 
 type Population = {
+    Year: int
     IsoCode : string
     Country : string
     LastUpdated : int
@@ -25,23 +26,30 @@ type ValidInput = {
     IsoCode : string 
 }
 
-type ValidationError =
-| MissingData of name: string
-
 let removeComma(s: string) = s.Replace(",", "")
 let toInt(s: string) = Int32.Parse(s)
+
+let loadCSVData (filePath: string) : CsvFile =
+    CsvFile.Load(filePath)
 
 let load (files: string[])=
     files
     |> Seq.map CsvFile.Load
-    |> Seq.fold (fun acc file -> Seq.append acc file.Rows ) []
 
-let parse (data:seq<CsvRow>) = 
-    data
+let getHeader(file:CsvFile) =
+    match file.Headers with
+    | Some(value) -> value.[2]
+    | None -> ""
+
+let parse (file:CsvFile) =     
+    let header = getHeader file
+    let year = header.[..3] |> toInt
+    file.Rows 
     |> Seq.choose (fun line -> 
         match line.Columns with
         | [| isoCode; country; lastUpdate; population; area; landArea; densitySq; growthRate; worldPersnt; rank|] -> 
-            Some { 
+            Some {
+                Year = 0
                 IsoCode = isoCode
                 Country = country
                 LastUpdated = lastUpdate |> removeComma |> toInt
@@ -55,6 +63,7 @@ let parse (data:seq<CsvRow>) =
             }
         | _ -> None
     )
+    |> Seq.map(fun p -> { p with Year = year })
 
 let (|IsEmptyString|_|) (input:string) =
     if input.Trim() = "" then Some () else None
@@ -65,11 +74,11 @@ let (|IsNotInt|_|) (input:string) =
 
 let validateInput (args:string) = 
     if args <> "" then Ok args
-    else Error (MissingData "Invalid args")
+    else Error "Invalid args"
 
 let create isoInput = { IsoCode = isoInput }  
 
-let validate (argv: string) : Result<ValidInput,ValidationError list> =
+let validate (argv: string) =
   validation {
     let! isoCode = 
         argv 
@@ -82,32 +91,47 @@ let populationSeq(data: seq<Population>) =
     data
     |> Seq.map (fun p -> p.Population)     
 
-let populationHead(data: seq<Population>) =
-    data
-    |> populationSeq
-    |> Seq.head 
-
 let populationDelta(data:seq<Population>) = 
      data
     |> populationSeq
-    |> Seq.skip 1
     |> Seq.reduce(fun acc i -> acc - i) 
 
-let populationСhange(isoCode:Result<ValidInput,ValidationError list> ) (data: seq<Population>) =    
+let populationСhange (data: seq<Population>) =    
     data
-    |> Seq.filter(fun p -> p.IsoCode.Equals(isoCode))
     |> Seq.groupBy (fun p -> p.IsoCode) 
-    |> Seq.map (fun (key, value) -> { 
-       IsoCode = key 
-       PopulationDelta = populationDelta(value) 
-    })
+    |> Seq.map (fun (key, value) -> {
+        IsoCode = key
+        PopulationDelta = populationDelta(value)
+        } 
+    )
+
+let populationСhangeByIso(isoCode:string ) (data: seq<Population>) =    
+    data
+    |> Seq.filter(fun p -> p.IsoCode.Equals(isoCode, StringComparison.OrdinalIgnoreCase))
+    |> populationСhange
+
+let getInputFromConsole (data:seq<Population>) =
+    let codes = data |> Seq.map(fun p -> p.IsoCode)
+    codes
+    |> Seq.map (fun s -> s + " ")
+    |> String.concat "   "
+    |> printfn "%s"
+    printfn "\nPlease type country name or ALL:"
+    let input = Console.ReadLine()
+    if input.Equals("all", StringComparison.OrdinalIgnoreCase) then
+        populationСhange data
+    elif(Seq.exists(fun x -> String.Equals(x, input, StringComparison.OrdinalIgnoreCase)) codes) then
+        populationСhangeByIso input data 
+    else
+        seq{{ 
+            IsoCode = "Not found"
+            PopulationDelta = 0
+        }}
 
 [<EntryPoint>]
 let main argv =
-    let isoCode = validate argv[0]
     let path = Path.Combine(__SOURCE_DIRECTORY__, "resources")
-    load (Directory.GetFiles(path, "*.csv"))
-    |> parse
-    |> populationСhange isoCode
-    |> Seq.iter (fun p -> printfn "%A" p)
+    load (Directory.GetFiles(path, "*.csv")) |> Seq.fold (fun acc file -> file |> parse) Seq.empty<Population>  
+    |> getInputFromConsole    
+    |> Seq.iter (fun p -> printfn "%A" p)    
     0
